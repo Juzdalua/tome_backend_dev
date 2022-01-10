c7f41218948e2bb28bcceeeb89f8ec29599ff117
Jun <bearwcw@naver.com>
2022-01-11 00:09:20 +0900

    Update Init

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-// const express = require("express");
+import "dotenv/config"
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 4000;
 
-app.get("/", (req,res) => res.render("hi"));
+app.get("/", (req,res) => res.send("hi"));
 
 app.listen(PORT, () => console.log(`🚀 Connect PORT: ${PORT}. ✅`) );
